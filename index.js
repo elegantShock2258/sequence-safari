b85a8ab5fec04e5bef3781230db6625a6b8661ea
Ayush Chadha <elegantShock3307@gmail.com>
2023-05-06 13:33:40 +0545

    sort of done?

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ let sequencePrompt = document.getElementById('sequencePrompt')
 
 let blocks = []
 let sequence = []
+let sequenceBackup = []
 let letterSequence = []
 let colorSequence = []
 let sequenceFinsihed = true
@@ -44,11 +45,13 @@ let score = 0
 let highScore = 0
 let lives = 3
 
+let paused = false
+
 //update game
 function generateSequence() {
     let i = 0;
     let a = 360 * Math.random()
-    while (i != sequenceLenght) {
+    while (i != (sequenceLenght)) {
         let j = Math.floor(side * side * Math.random())
         if (j in snake) continue
         else {
@@ -59,12 +62,14 @@ function generateSequence() {
             i++
         }
     }
+    sequenceBackup = sequence
 
 }
 function placeSequence() {
     sequencePrompt.textContent = ''
-    for (let i = sequenceLenght - 1; i > 0; i--) {
-        cells[sequence[i]].innerText = String.fromCharCode(letterSequence[i])
+    for (let i = sequenceLenght-1; i >= 0; i--) {
+        console.log(sequence[i])
+        cells[sequence[i]].innerHTML = "<span>" + String.fromCharCode(letterSequence[i]) + "</span>"
         cells[sequence[i]].style.background = `hsl(${colorSequence[i]},100%,50%)`
         cells[sequence[i]].classList.add("block")
 
@@ -74,8 +79,6 @@ function placeSequence() {
         div.style.background = `hsl(${colorSequence[i]},100%,50%)`
 
         sequencePrompt.appendChild(div)
-
-
     }
 }
 function updateGame() {
@@ -90,11 +93,11 @@ function updateGame() {
     document.getElementById("lives").textContent = "Lives: " + lives
 
     barObject.style.width = `${currentTime * 100 / defaultTime}%`
-
+    if (currentTime == defaultTime) endgame()
 }
 
 // grid
-function setUpGrid(width = 10, sqaureWidth = 70) {
+function setUpGrid(width = 10, sqaureWidth = 80) {
     let numSquares = (sqaureWidth / width) * (sqaureWidth / width)
     for (let i = 0; i < numSquares; i++) {
         let div = document.createElement('div')
@@ -114,11 +117,11 @@ function updateSnake() {
     })
 
     cells[snake[snake.length - 1]].classList.add('tail')
-    cells[snake[snake.length - 1]].innerText = '(‿ˠ‿)'
+    cells[snake[snake.length - 1]].innerHTML = '<span>(‿ˠ‿)</span>'
 
     cells[snake[0]].classList.add('snake')
     cells[snake[0]].classList.add('head')
-    cells[snake[0]].innerText = ':D'
+    cells[snake[0]].innerHTML = '<span>:D</span>'
 }
 
 
@@ -128,6 +131,7 @@ let displacement = 1
 
 let snake = []
 function setup() {
+
     snake = [15, 29, 43, 57]
     if (localStorage.getItem('highScore') === null) sessionStorage.setItem('highScore', 0)
 
@@ -142,35 +146,50 @@ function setup() {
             displacement = 1
         } else if (e.key === "ArrowUp") {
             displacement = -1
+        } else if (e.key === "p") {
+            paused = !paused
         }
     })
 
-    setUpGrid(5, 70)
-    side = 70 / 5
+    setUpGrid(2, 60)
+    side = 60 / 2
     for (let i = 0; i < side * side; i++)
         cells[i] = document.getElementById("square" + i)
 
     updateSnake(snake)
+
+    let str = "linear-gradient(0.25turn," + Array(sequence.length).fill("hsl(0,100%,50%)").toLocaleString() + ")"
+    document.body.style.background = str
+
+
 }
 
 function loop() {
     var loop = setInterval(() => {
-        if (lives > 0) {
-            move(displacement)
-            updateGame()
-        } else  if(lives === 0) {
-            console.log("you lost!")
-            // grow(false)
-            clearInterval(loop)
+        if (!paused) {
+            if (lives > 0) {
+                updateGame()
+                move(displacement)
+            } else if (lives === 0) {
+                console.log("you lost!")
+                // grow(false)
+                clearInterval(loop)
+            }
+        } else {
+            return;
         }
     }, 100)
 }
 
 //movement 
 function move(displacement) {
-    let collide = checkCollision(snake[0], displacement)
+    let collide = true
+    snake.forEach((i) => {
+        collide &= checkCollision(i, displacement)
+    })
     if (!collide) {
-        cells[snake[0]].innerText = ''
+        // cells[snake[0]].innerText = ''
+        cells[snake[0]].innerText = cells[snake[0]].innerText.replace(":D", '')
         cells[snake[0]].classList.remove('head')
         const tail = snake.pop()
         cells[tail].classList.remove('snake')
@@ -178,18 +197,13 @@ function move(displacement) {
         cells[tail].innerText = ''
         snake.unshift((snake[0] + displacement))
         cells[snake[snake.length - 1]].classList.add('tail')
-        cells[snake[snake.length - 1]].innerText = '(‿ˠ‿)'
+        cells[snake[snake.length - 1]].innerHTML = '<span>(‿ˠ‿)</span>'
 
         cells[snake[0]].classList.add('snake')
         cells[snake[0]].classList.add('head')
-        cells[snake[0]].innerText = ':D'
+        cells[snake[0]].innerHTML = '<span>:D</span>'
     } else if (collide) {
         endgame()
-        displacement = 1
-        sequenceFinsihed = true
-        playground.innerHTML = ''
-        setup()
-        updateGame()
     }
 
     return collide;
@@ -203,7 +217,7 @@ function checkCollision(coord, displacement) {
 
     let x = x0 + xd
     let y = y0 + yd
-    console.log(x, y)
+    console.log(x*side + y, sequence)
     if (y > side - 1 || y < 0) {
         console.log("wall collision")
         return true
@@ -214,17 +228,22 @@ function checkCollision(coord, displacement) {
     }
     const grid = x * side + y
     if (sequence.includes(grid)) {
-        console.log("block collision")
+        console.log("block collision " + grid)
         return blockCollided(grid)
-    } else if (snake.includes(grid)) {
-        console.log("snake collision")
-        return true
     }
+    // if (snake.includes(grid)) {
+    //     console.log("snake collision")
+    //     return true
+    // }
     return false
 }
 function blockCollided(i) {
+
+    let color = sequenceBackup.indexOf(i)
     let last = sequence.pop()
+
     console.log(i, last, sequence)
+
     if (i == last && sequence.length > 0) {
         cells[i].classList.add('square')
         cells[i].classList.remove('block')
@@ -232,7 +251,11 @@ function blockCollided(i) {
         cells[i].innerText = ''
 
         grow(true)
-
+        let k = 0
+        let str = "linear-gradient(0.25turn," + colorSequence.slice(-(sequenceLenght - color)).map((e) => "hsl(" + e + ",100%,50%) " + ((++k) / sequenceLenght) * 100 + "%").toLocaleString() + "," + Array(sequence.length).fill("hsl(" + colorSequence[sequenceLenght - color] + ",100%,50%)").toLocaleString() + ")"
+        document.body.classList.remove("transistion")
+        document.body.classList.add("transistion")
+        document.body.style.background = str
         sequencePrompt.removeChild(sequencePrompt.firstChild)
         // increment bg 
 
@@ -246,18 +269,22 @@ function blockCollided(i) {
         grow(true)
         console.log("sequence finsihed!")
 
-        sequencePrompt.removeChild(sequencePrompt.firstChild)
+        let str = "linear-gradient(0.25turn," + colorSequence.slice(-(sequenceLenght - color)).map((e) => "hsl(" + e + ",100%,50%)").toLocaleString() + "," + Array(sequence.length).fill("black").toLocaleString() + ")"
+        document.body.style.background = str
+
+        if (sequencePrompt.firstChild != null)
+            sequencePrompt.removeChild(sequencePrompt.firstChild)
         // increment bg 
 
         sequenceFinsihed = true
         score++
-        defaultTime += 5000 //+5s
-
+        defaultTime += 30000 //+5s
         updateGame()
         return false;
     }
     else {
         console.log("wrong sequence")
+        lives = 0
         return true;
     }
 }
@@ -302,9 +329,10 @@ function grow(grow) {
 
 function endgame() {
     lives--;
+    displacement = 1
     if (lives > 0) {
         if (score > highScore) sessionStorage.setItem("highScore", highScore)
-    } else if(lives = 0){
+    } else if (lives == 0) {
         return;
     }
 }
